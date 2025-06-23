5b4265fe74b20dfb4e2e0762f993853074b70c8f
Dawid Wojtyca <dawid.wojtyca@thomsonreuters.com>
2025-06-23 15:25:45 +0200

    revert changes from eslint config, add new package

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -27,6 +27,6 @@ export const config = [
     },
   },
   {
-    ignores: ["dist/**", "something/else"],
+    ignores: ["dist/**"],
   },
 ];
---
+++ packages/foo/src/foo.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { Button } from "@repo/ui/button";
+
+export const FooButton = () => {
+  return <Button appName="TEST">FOOOO</Button>;
+};
